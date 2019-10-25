91ff9127824b30ffa948aa48cd1106657ebc5c31
kooyunmo <mpbb03@naver.com>
2019-10-25 19:08:37 +0900

    fix some pylint errrors

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -1,3 +1,8 @@
+"""Django admin
+
+In this module, you need to declare the models that you want to see in the Django admin page.
+"""
+
 from django.contrib import admin
 from .models import Profile
 
--- backend/account/apps.py
+++ backend/account/apps.py
@@ -1,5 +1,11 @@
+"""Django App
+This is the Django application config file for Account
+"""
+
 from django.apps import AppConfig
 
-
 class AccountConfig(AppConfig):
+    """Account Config
+    Define the name of the app
+    """
     name = 'account'
