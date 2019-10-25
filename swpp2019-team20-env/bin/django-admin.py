cbe6cfddc94e391833186461d73e90c84d1511ce
Yongun Seong <nevivurn@riseup.net>
2019-10-25 16:48:42 +0900

    backend: remove venv

--- swpp2019-team20-env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/Kooyunmo/Documents/swpp/swpp2019-team20/swpp2019-team20-env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
