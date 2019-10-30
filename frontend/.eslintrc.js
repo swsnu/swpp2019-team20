0a49f6ac7e8a70312fc1ae90ea0ff925d329e050
nungnung <meun3170@daum.net>
2019-10-30 20:54:07 +0900

    fix eslint import rule

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -24,9 +24,6 @@ module.exports = {
         "react"
     ],
     "rules": {
-	"import/no-unresolved": [
-      2, 
-      { "caseSensitive": false }
-   ]
-    }
+    	'import/no-unresolved': "off",
+    },
 };
