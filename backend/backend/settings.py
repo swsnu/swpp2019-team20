eef84f13390bb35b593f92088cb78c3333783179
nungnung <meun3170@daum.net>
2019-10-31 17:26:55 +0900

    create review model

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'review.apps.ReviewConfig',
     'loan.apps.LoanConfig',
     'account.apps.AccountConfig',
     'django.contrib.admin',
