b8606347291e5e545a051c10a1fe83e9cde714e1
coffeetea99 <42795430+coffeetea99@users.noreply.github.com>
2019-11-02 16:08:23 +0900

    Loans create (#28)

--- backend/loan/models.py
+++ backend/loan/models.py
@@ -1,6 +1,6 @@
-#from django.db import models
+from django.db import models
 #from django.utils import timezone
-#from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -10,26 +10,28 @@
 #    date = models.DateTimeField(auto_now_add=True)
 
 
-#class Loan(models.Model):
-#    num_members = models.PositiveSmallIntegerField(min_value=2)
-#    deadline = models.DateTimeField()
-#    total_money = models.DecimalField(max_digits=8, decimal_places=2)
-#    alert_frequency = models.CharField()
-#    apply_interest = models.BooleanField()
-#    interest_rate = models.FloatField()
-#    completed = models.BooleanField()
-#    expected_date = models.DateTimeField()
-#    completed_date = models.DateTimeField()
-#    average_rating = models.FloatField()
-#    registered_date = models.DateTimeField(auto_now_daa=True)
+class Loan(models.Model):
+    num_members = models.PositiveSmallIntegerField()
+    deadline = models.DateTimeField()
+    total_money = models.DecimalField(max_digits=8, decimal_places=2)
+    alert_frequency = models.CharField(max_length=16)
+    apply_interest = models.BooleanField()
+    interest_type = models.CharField(max_length=16, null=True)
+    interest_rate = models.FloatField()
+    completed = models.BooleanField()
+    expected_date = models.DateTimeField(null=True)
+    completed_date = models.DateTimeField(null=True)
+    registered_date = models.DateTimeField(auto_now_add=True)
 
 
-#class Transaction(models.Model):
-#    loan_id = models.ForeignKey(Loan, on_delete=models.CASCADE)
-#    lender_id = models.ForeignKey(User, on_delete=models.CASCADE)
-#    borrower_id = models.ForeignKey(User, on_delete=models.CASCADE)
-#    money = models.DecimalField(max_digits=8, decimal_places=2)
-#    completed = models.BooleanField()
-#    completed_date = models.DateTimeField()
-#    lender_confirm = models.BooleanField()
-#    borrower_confirm = models.BooleanField()
+class Transaction(models.Model):
+    loan = models.ForeignKey(Loan, on_delete=models.CASCADE)
+    lender = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='lended_transaction')
+    borrower = models.ForeignKey(User, on_delete=models.CASCADE,
+                                 related_name='borrowed_transaction')
+    money = models.DecimalField(max_digits=8, decimal_places=2)
+    completed = models.BooleanField()
+    completed_date = models.DateTimeField()
+    lender_confirm = models.BooleanField()
+    borrower_confirm = models.BooleanField()
--- backend/loan/tests.py
+++ backend/loan/tests.py
@@ -1,3 +1,78 @@
-#from django.test import TestCase
+#import json
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+from .models import Loan
 
 # Create your tests here.
+
+class LoanTestCase(TestCase):
+    def test_loan(self):
+        client = Client()
+
+        # test - no such api
+        response = client.delete('/loan/loan')
+        self.assertEqual(response.status_code, 405)
+
+        # test - not logged in
+        response = client.post('/loan/loan')
+        self.assertEqual(response.status_code, 401)
+
+        # login
+        User.objects.create_user(username='username', password='password')
+        client.login(username='username', password='password')
+
+        # test - no request body
+        response = client.post('/loan/loan')
+        self.assertEqual(response.status_code, 400)
+
+        # test - body is not json
+        response = client.post('/loan/loan',
+                               'This is a string',
+                               content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+
+        # make invalid data array
+        invalid_data = [
+            {'participants': None,
+             'deadline': '2019-12-31T23:59:59Z', 'interest_rate': 1.5,
+             'interest_type': 'day', 'alert_frequency': 'high'},
+            {'participants': [{'id': 1, 'paid_money': 50}, {'id': 2, 'paid_money': 100}],
+             'deadline': None, 'interest_rate': 1.5,
+             'interest_type': 'day', 'alert_frequency': 'high'},
+            {'participants': [{'id': 1, 'paid_money': 50}, {'id': 2, 'paid_money': 100}],
+             'deadline': '2019-12-31T23:59:59Z', 'interest_rate': None,
+             'interest_type': 'day', 'alert_frequency': 'high'},
+            {'participants': [{'id': 1, 'paid_money': 50}, {'id': 2, 'paid_money': 100}],
+             'deadline': '2019-12-31T23:59:59Z', 'interest_rate': 1.5,
+             'interest_type': None, 'alert_frequency': 'high'},
+            {'participants': [{'id': 1, 'paid_money': 50}, {'id': 2, 'paid_money': 100}],
+             'deadline': '2019-12-31T23:59:59Z', 'interest_rate': 1.5,
+             'interest_type': 'day', 'alert_frequency': None},
+        ]
+
+        # test - invalid input data
+        for invalid_datum in invalid_data:
+            response = client.post('/loan/loan',
+                                   invalid_datum,
+                                   content_type='application/json')
+            self.assertEqual(response.status_code, 400)
+
+        #make valid data
+        valid_data = {'participants': [{'id': 1, 'paid_money': 50}, {'id': 2, 'paid_money': 100}],
+                      'deadline': '2019-12-31T23:59:59Z', 'interest_rate': 1.5,
+                      'interest_type': 'day', 'alert_frequency': 'high'}
+
+        # test - valid input data
+        response = client.post('/loan/loan',
+                               valid_data,
+                               content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+
+        # test - correct data
+        try:
+            loan = Loan.objects.get()
+        except ObjectDoesNotExist:
+            self.fail("The data is not stored")
+        if loan.total_money != 150:
+            self.fail("The data is not stored correctly")
