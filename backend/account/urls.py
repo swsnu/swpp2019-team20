039adb99030a6342bf5dc334b41107b340a3d66f
nungnung <meun3170@daum.net>
2019-11-02 20:48:38 +0900

    fix conflict

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -8,9 +8,6 @@ urlpatterns = [
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
     path('signout', views.signout, name='signout'),
-<<<<<<< HEAD
     path('by-name/<str:username>', views.by_name, name='by name'),
-=======
->>>>>>> a6f4d5a950d554cb7470866490a2a2b214e7653d
     path('user/<int:pk>/', views.profile, name='profile'),
 ]
