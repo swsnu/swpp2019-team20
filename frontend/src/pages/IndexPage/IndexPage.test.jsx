cb871fed84e0d89c04104c42a71c81779318df1e
kooyunmo <mpbb03@naver.com>
2019-11-03 14:45:49 +0900

    coverage, lint: 100% for the assigned pages

---
+++ frontend/src/pages/IndexPage/IndexPage.test.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import IndexPage from './IndexPage';
+
+describe('IndexPage', () => {
+    test('renders without errors', () => {
+        const component = shallow(<IndexPage />);
+        expect(component.length).toBe(1);
+    });
+});
