a2ec45733527772f2cad07a991b06a46919cc417
kooyunmo <mpbb03@naver.com>
2019-11-05 00:20:05 +0900

    fix eslint errors

--- frontend/src/pages/LoginPage/LoginPage.test.jsx
+++ frontend/src/pages/LoginPage/LoginPage.test.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
 import { shallow, mount } from 'enzyme';
 import LoginPage from './LoginPage';
 
@@ -19,18 +18,18 @@ describe('LoginPage', () => {
   });
 
   test('email block marked as invalid with wrong input', () => {
-    const email = "wrongemailformat";
-    const password = "password";
+    const email = 'wrongemailformat';
+    const password = 'password';
     const component = mount(login);
-    const email_input_wrapper = component.find('#login-email-input input');
-    const password_input_wrapper = component.find('#login-password-input input');
-    const submit_button = component.find('button');
+    const emailInputWrapper = component.find('#login-email-input input');
+    const passwordInputWrapper = component.find('#login-password-input input');
+    const submitButton = component.find('button');
 
-    expect(submit_button.length).toBe(1);
+    expect(submitButton.length).toBe(1);
 
-    email_input_wrapper.simulate('change', { target: {value: email} });
-    password_input_wrapper.simulate('change', { target: { value: password } });
+    emailInputWrapper.simulate('change', { target: { value: email } });
+    passwordInputWrapper.simulate('change', { target: { value: password } });
 
-    submit_button.simulate('submit');
+    submitButton.simulate('submit');
   });
 });
