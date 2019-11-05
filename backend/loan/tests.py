d7b8ce8ea3dc343182b8bb964da3079e7be0258b
nungnung <meun3170@daum.net>
2019-11-05 19:30:04 +0900

    get laon list test

--- backend/loan/tests.py
+++ backend/loan/tests.py
@@ -22,6 +22,10 @@ class LoanTestCase(TestCase):
         User.objects.create_user(username='username', password='password')
         client.login(username='username', password='password')
 
+        # test - get
+        response = client.get('/loan/loan')
+        self.assertEqual(response.status_code, 200)
+
         # test - no request body
         response = client.post('/loan/loan')
         self.assertEqual(response.status_code, 400)
