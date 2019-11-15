efd3d01c3cc896b0608f8247e5421e20038f8218
kooyunmo <mpbb03@naver.com>
2019-11-16 04:47:01 +0900

    all test coverage 100%

--- frontend/src/App.test.jsx
+++ frontend/src/App.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import ReactDOM from 'react-dom';
 import App, { AppContext } from './App';
@@ -16,18 +16,39 @@ describe('<App />', () => {
   const setState = jest.fn();
   const useStateSpy = jest.spyOn(React, 'useState');
   useStateSpy.mockImplementation((init) => [init, setState]);
+  let getLoginSpy;
+
+  beforeEach(() => {
+    getLoginSpy = jest.spyOn(window, 'fetch')
+      // eslint-disable-next-line
+      .mockImplementation((url) => {
+        const result = {
+          status: 204,
+        };
+        return Promise.resolve(result);
+      });
+  });
 
   afterEach(() => {
     jest.clearAllMocks();
   });
 
   test('when loggedIn is true', () => {
+    getLoginSpy = jest.spyOn(window, 'fetch')
+      // eslint-disable-next-line
+      .mockImplementation((url) => {
+        const result = {
+          status: 403,
+        };
+        return Promise.resolve(result);
+      });
+
     const [user, setUser] = useStateSpy({
       loggedIn: true,
       username: '',
     });
 
-    const component = shallow(
+    const component = mount(
       <AppContext.Provider value={{ user, setUser }}>
         <App />
       </AppContext.Provider>,
@@ -41,7 +62,7 @@ describe('<App />', () => {
       username: '',
     });
 
-    const component = shallow(
+    const component = mount(
       <AppContext.Provider value={{ user, setUser }}>
         <App />
       </AppContext.Provider>,
