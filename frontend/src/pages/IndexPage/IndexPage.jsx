579f754c0ed995b0c4f1b5629e172e71014d2134
kooyunmo <mpbb03@naver.com>
2019-11-15 14:58:04 +0900

    fix configs

--- frontend/src/pages/IndexPage/IndexPage.jsx
+++ frontend/src/pages/IndexPage/IndexPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Base from '../../components/Base/Base';
 
-const IndexPage = () => <Base input={<div>Sign In Page</div>} />;
+const IndexPage = () => <div>Sign In Page</div>;
 
 export default IndexPage;
