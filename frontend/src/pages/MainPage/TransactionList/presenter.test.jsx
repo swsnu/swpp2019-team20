b8c8bb7db5fc384e636c911ac9bd57b8ab38b06b
nungnung <meun3170@daum.net>
2019-11-23 16:25:38 +0900

    upgrade coverage

--- frontend/src/pages/MainPage/TransactionList/presenter.test.jsx
+++ frontend/src/pages/MainPage/TransactionList/presenter.test.jsx
@@ -25,4 +25,25 @@ describe('<LatestOrders />', () => {
     const component = shallow(<LatestOrders TxList={mockData} />);
     expect(component.length).toEqual(1);
   });
+  it('should render without error', () => {
+    let mockData = [];
+    act(() => {
+      mockData = [
+        {
+          id: 1,
+          lender: 'h',
+          borrower: 'h',
+          money: '1.00',
+          completed: true,
+          completed_date: '2019-11-15T23:59:59Z',
+          lender_confirm: true,
+          borrower_confirm: true,
+          loan_id: 1,
+          borrower_id: 2,
+          lender_id: 2,
+        }];
+    });
+    const component = shallow(<LatestOrders TxList={mockData} />);
+    expect(component.length).toEqual(1);
+  });
 });
