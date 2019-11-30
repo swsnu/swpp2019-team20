e30b7b057754166bc7eb6a77cde5785e68d99774
coffeetea99 <coffeetea99iro@gmail.com>
2019-11-30 14:55:35 +0900

    done click to profile page

--- frontend/src/components/Base/Base.jsx
+++ frontend/src/components/Base/Base.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { useHistory } from 'react-router';
 import theme from './theme';
 import Header from '../subcomponents/Header/Header';
 import Footer from '../subcomponents/Footer/Footer';
@@ -9,7 +10,13 @@ import SearchBar from '../subcomponents/SearchBar/SearchBar';
 
 function Base(Component) {
   function BaseBase(props) {
-    const setUser = () => 'user';
+    const history = useHistory();
+
+    const setUser = (user) => {
+      if ( user !== null ) {
+        history.push(`/profile/${user.id}`);
+      }
+    }
     return (
       <ThemeProvider theme={theme}>
         <CssBaseline />
