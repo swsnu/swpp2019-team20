424b67d96356822216c09aef266673d482f5e676
kooyunmo <mpbb03@naver.com>
2019-12-05 01:13:32 +0900

    fix pylint error

--- backend/backend/routing.py
+++ backend/backend/routing.py
@@ -2,7 +2,7 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import loan.routing
 
-application = ProtocolTypeRouter({
+application = ProtocolTypeRouter({      # pylint: disable=invalid-name
     # (http->django views is added by default)
     'websocket': AuthMiddlewareStack(
         URLRouter(
