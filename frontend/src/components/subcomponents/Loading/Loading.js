915348081026ccd95796eaf2fa99f52ac2f1e252
nungnung <meun3170@daum.net>
2019-12-10 18:55:57 +0900

    add loading image and string

--- frontend/src/components/subcomponents/Loading/Loading.js
+++ frontend/src/components/subcomponents/Loading/Loading.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React from 'react';
 
-const Loading = () => <div>Loading...</div>;
+const Loading = () => {
+  const loadingImage = 'https://cdn.dribbble.com/users/1283749/screenshots/4050901/honey-coin-walk.gif';
+  const loadingString = require('./loading.gif');
+  return (
+    <>
+      <div style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}>
+        <img src={loadingImage} align="center" className="center"/>
+      </div>
+      <div style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}>
+        <img src={loadingString} align="center" className="center"/>
+      </div>
+    </>
+  )
+};
 
 export default Loading;
-
-// https://cdn.dribbble.com/users/1283749/screenshots/4050901/honey-coin-walk.gif
