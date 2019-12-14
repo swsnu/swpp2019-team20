8d1ba0a1a4e48a491b3afc73a510cb4f73ff9a52
kooyunmo <mpbb03@naver.com>
2019-12-14 12:01:59 +0900

    fix code smells

--- backend/loan/models.py
+++ backend/loan/models.py
@@ -1,13 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-
-#class Deposit(models.Model):
-#    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-#    money = models.DecimalField(max_digits=8, decimal_places=2)
-#    date = models.DateTimeField(auto_now_add=True)
-
 
 class Loan(models.Model):
     progress_rate = models.FloatField(default=0)
--- backend/review/models.py
+++ backend/review/models.py
@@ -8,7 +8,6 @@ class Review(models.Model):
     For User 2 User Feedback System,
     which includes free text and evaluated credit rating.
     """
-    # reviewer = models.ForeignKey(User, related_name='reviewer', on_delete=models.CASCADE)
     reviewee = models.ForeignKey(User, related_name='reviewee', on_delete=models.CASCADE)
     rating = models.FloatField()
     content = models.TextField()
