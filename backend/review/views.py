1f0331bfa15add8b0559a9ec2989b0a9c947ee91
nungnung <meun3170@daum.net>
2019-12-14 10:46:23 +0900

    for commit

--- backend/review/views.py
+++ backend/review/views.py
@@ -27,7 +27,7 @@ def user_rating(request, reviewee_id):
         reviewee_user = get_object_or_404(User, pk=reviewee_id)
         review_list = Review.objects.filter(reviewee=reviewee_user)
 
-        if len(review_list) != 0:
+        if len(review_list) > 0:
             my_rating = 0
             for review in review_list:
                 my_rating += review.rating
