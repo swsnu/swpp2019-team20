38afa959f2300e951dde968ff1ef69a1fec95b73
coffeetea99 <coffeetea99iro@gmail.com>
2019-12-14 14:16:03 +0900

    merge conflict

--- frontend/src/pages/MainPage/CreateLoan/CreateLoan.jsx
+++ frontend/src/pages/MainPage/CreateLoan/CreateLoan.jsx
@@ -111,8 +111,7 @@ const CreateLoan = () => {
       const currentID = participants[i].id;
       // currentID < 0 : do nothing
       if (currentID === 0) {
-        // eslint-disable-next-line
-        window.alert(`username on row ${real+1} is empty!`);
+        swal(`username on row ${real + 1} is empty!`);
         return;
       }
 
